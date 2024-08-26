28efe99bdaae8884db7ae96300893869833ca588
Tamir Duberstein <tamird@gmail.com>
2024-08-26 13:09:13 -0400

    Replace futures::ThreadPool with rayon::ThreadPool
    Use `ThreadPool::scope` to avoid unnecessary `Arc`s.
    
    This breaks API: `Job::add_task`'s argument no longer takes a
    `futures::task::Context` argument.

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,7 @@
 use std::collections::BTreeSet;
-use std::sync::{Arc, Mutex};
+use std::convert::TryInto as _;
+use std::sync::Mutex;
 use std::time::Instant;
-
-use futures::future;
-use futures::task::SpawnExt;
 
 fn progress_bar_style(task_count: usize) -> indicatif::ProgressStyle {
     let task_count_digits = task_count.to_string().len();
@@ -21,11 +19,11 @@ struct TaskMonitorId((Instant, String, usize));
 struct TaskMonitor {
     counter: usize,
     tasks: BTreeSet<TaskMonitorId>,
-    progress_bar: Arc<indicatif::ProgressBar>,
+    progress_bar: indicatif::ProgressBar,
 }
 
 impl TaskMonitor {
-    fn new(progress_bar: Arc<indicatif::ProgressBar>) -> TaskMonitor {
+    fn new(progress_bar: indicatif::ProgressBar) -> TaskMonitor {
         TaskMonitor {
             counter: 0,
             tasks: BTreeSet::new(),
@@ -64,7 +62,7 @@ impl TaskMonitor {
 
 pub struct Pool {
     thread_count: Option<usize>,
-    thread_pool: Option<futures::executor::ThreadPool>,
+    thread_pool: Option<rayon::ThreadPool>,
     quiet: bool,
 }
 
@@ -99,80 +97,77 @@ impl Pool {
         self.quiet = quiet;
     }
 
-    fn thread_pool(&mut self) -> &mut futures::executor::ThreadPool {
+    fn thread_pool(&mut self) -> &mut rayon::ThreadPool {
         if self.thread_pool.is_some() {
             self.thread_pool.as_mut().unwrap()
         } else {
-            let pool = match self.thread_count {
-                Some(count) => futures::executor::ThreadPool::builder()
-                    .pool_size(count)
-                    .create(),
-                None => futures::executor::ThreadPool::new(),
+            let builder = rayon::ThreadPoolBuilder::new();
+            let builder = match self.thread_count {
+                Some(count) => builder.num_threads(count),
+                None => builder,
             };
 
-            self.thread_pool =
-                Some(pool.unwrap_or_else(|err| panic!("failed to create job pool: {}", err)));
+            self.thread_pool = Some(
+                builder
+                    .build()
+                    .unwrap_or_else(|err| panic!("failed to create job pool: {}", err)),
+            );
             self.thread_pool.as_mut().unwrap()
         }
     }
 
-    pub fn execute<T: Send + 'static, E: Send + 'static>(
-        &mut self,
-        mut job: Job<T, E>,
-    ) -> ExecutionResults<T, E> {
-        let task_count = job.tasks.len();
-        let pb = Arc::new(if self.quiet {
+    pub fn execute<T: Send, E: Send>(&mut self, mut job: Job<T, E>) -> ExecutionResults<T, E> {
+        let mut task_monitor = TaskMonitor::new(if self.quiet {
             indicatif::ProgressBar::hidden()
         } else {
-            indicatif::ProgressBar::new(task_count as u64)
+            let task_count = job.tasks.len();
+            let pb = indicatif::ProgressBar::new(task_count.try_into().unwrap());
+            pb.set_style(progress_bar_style(task_count));
+            pb.set_prefix(job.name.clone());
+            pb.enable_steady_tick(1000);
+            pb
         });
-        pb.set_style(progress_bar_style(task_count));
-        pb.set_prefix(job.name.clone());
-        pb.enable_steady_tick(1000);
 
         tracing::span!(tracing::Level::INFO, "Pool::execute", name = job.name).in_scope(|| {
-            let task_monitor = Arc::new(Mutex::new(TaskMonitor::new(Arc::clone(&pb))));
-            let handles: Vec<_> = job
-                .tasks
-                .drain(..)
-                .map(|task| {
-                    let task_monitor = Arc::clone(&task_monitor);
-                    self.thread_pool()
-                        .spawn_with_handle(future::lazy(move |context| {
-                            tracing::span!(tracing::Level::INFO, "Task::task", name = task.name)
-                                .in_scope(|| {
-                                    let task_id =
-                                        task_monitor.lock().unwrap().started(task.name.clone());
-                                    let result = (task.task)(context);
-                                    task_monitor.lock().unwrap().finished(task_id);
-                                    (task.name, result)
-                                })
-                        }))
-                        .expect("failed to spawn job")
-                })
-                .collect();
-
-            let handles = futures::executor::block_on(future::join_all(handles));
-            pb.finish();
-
             let mut successful = Vec::new();
             let mut failed = Vec::new();
+            {
+                let state = Mutex::new((&mut task_monitor, &mut successful, &mut failed));
+                self.thread_pool().scope(|scope| {
+                    for task in job.tasks.drain(..) {
+                        scope.spawn(|_| {
+                            tracing::span!(tracing::Level::INFO, "Task::task", name = task.name)
+                                .in_scope(|| {
+                                    let task_id = {
+                                        let mut guard = state.lock().unwrap();
+                                        let (task_monitor, _, _) = &mut *guard;
+                                        task_monitor.started(task.name.clone())
+                                    };
 
-            for (task_name, result) in handles {
-                match result {
-                    Ok(result) => {
-                        successful.push(ExecutionResult {
-                            name: task_name,
-                            result,
-                        });
+                                    let result = (task.task)();
+
+                                    let mut guard = state.lock().unwrap();
+                                    let (task_monitor, successful, failed) = &mut *guard;
+                                    task_monitor.finished(task_id);
+                                    match result {
+                                        Ok(result) => {
+                                            successful.push(ExecutionResult {
+                                                name: task.name,
+                                                result,
+                                            });
+                                        }
+
+                                        Err(err) => failed.push(ExecutionResult {
+                                            name: task.name,
+                                            result: err,
+                                        }),
+                                    }
+                                })
+                        })
                     }
-
-                    Err(err) => failed.push(ExecutionResult {
-                        name: task_name,
-                        result: err,
-                    }),
-                }
+                });
             }
+            task_monitor.progress_bar.finish();
 
             ExecutionResults { successful, failed }
         })
@@ -192,10 +187,7 @@ impl<T: Send, E: Send> Job<T, E> {
         }
     }
 
-    pub fn add_task<
-        N: Into<String>,
-        F: FnOnce(&futures::task::Context) -> Result<T, E> + Send + 'static,
-    >(
+    pub fn add_task<N: Into<String>, F: FnOnce() -> Result<T, E> + Send + 'static>(
         &mut self,
         name: N,
         task: F,
@@ -210,8 +202,7 @@ impl<T: Send, E: Send> Job<T, E> {
 
 struct Task<T: Send, E: Send> {
     name: String,
-    #[allow(clippy::type_complexity)]
-    task: Box<dyn FnOnce(&futures::task::Context) -> Result<T, E> + Send>,
+    task: Box<dyn FnOnce() -> Result<T, E> + Send>,
 }
 
 #[cfg(test)]
@@ -225,26 +216,27 @@ mod tests {
         let (send_3, recv_3) = std::sync::mpsc::channel();
 
         let mut job = Job::with_name("forward");
-        job.add_task("first", move |_| {
+        job.add_task("first", move || {
             let data = recv_1.recv().unwrap();
             send_2.send(data + 1).unwrap();
             Ok(data)
         });
 
-        job.add_task("second", move |_| {
+        job.add_task("second", move || {
             let data = recv_2.recv().unwrap();
             send_3.send(data + 1).unwrap();
             Ok(data)
         });
 
-        job.add_task("third", move |_| {
+        job.add_task("third", move || {
             let data = recv_3.recv().unwrap();
             Err(format!("{}", data + 1))
         });
 
         send_1.send(0).unwrap();
 
-        let mut pool = Pool::with_size(1);
+        // Dispatch order is not guaranteed, must schedule all at once to avoid deadlock.
+        let mut pool = Pool::with_size(job.tasks.len());
         let results = pool.execute(job);
 
         assert_eq!(results.successful.len(), 2);
